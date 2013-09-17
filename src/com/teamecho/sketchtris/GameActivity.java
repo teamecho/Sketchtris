3475cb0b44b404bf364eda7b3ca86910dfda64a5
Jess Bodzo <jess@sjbodzo.com>
2013-09-17 19:59:23 -0400

    Committing changes so I can go to my branch. Nothing new

---
+++ src/com/teamecho/sketchtris/GameActivity.java
@@ -0,0 +1,15 @@
+package com.teamecho.sketchtris;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class GameActivity extends Activity {
+
+	
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_gameplay);
+	}
+	
+}
