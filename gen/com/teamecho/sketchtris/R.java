e9a253bc5ce605d68fe1a151308298f9594e62d4
Prometheus <jess@sjbodzo.com>
2013-10-04 00:55:30 -0400

    fixed realtime error

--- gen/com/teamecho/sketchtris/R.java
+++ gen/com/teamecho/sketchtris/R.java
@@ -25,13 +25,13 @@ public final class R {
     }
     public static final class id {
         public static final int action_settings=0x7f090003;
-        public static final int gestureOverlayView1=0x7f090000;
-        public static final int playgamebutton=0x7f090002;
-        public static final int title=0x7f090001;
+        public static final int baseLayout=0x7f090002;
+        public static final int playgamebutton=0x7f090001;
+        public static final int title=0x7f090000;
     }
     public static final class layout {
-        public static final int activity_gameplay=0x7f030000;
-        public static final int activity_main=0x7f030001;
+        public static final int activity_main=0x7f030000;
+        public static final int base_layout=0x7f030001;
     }
     public static final class menu {
         public static final int main=0x7f080000;
--- src/com/teamecho/sketchtris/GameActivity.java
+++ src/com/teamecho/sketchtris/GameActivity.java
@@ -1,7 +1,6 @@
 package com.teamecho.sketchtris;
 
 import java.util.ArrayList;
-
 import android.app.Activity;
 import android.gesture.Gesture;
 import android.gesture.GestureLibraries;
@@ -10,48 +9,62 @@ import android.gesture.GestureOverlayView;
 import android.gesture.GestureOverlayView.OnGesturePerformedListener;
 import android.gesture.Prediction;
 import android.os.Bundle;
-import android.widget.Toast;
+import android.util.Log;
+import android.widget.FrameLayout;
+import android.widget.Toast; 
 
-public class GameActivity extends Activity implements OnGesturePerformedListener {
-	GestureLibrary myGestureLib; //this will have the defined gestures stored in it, so it knows what to look for
+public class GameActivity extends Activity {
+	private SketchtrisView gameplayView; 
+	private GestureLibrary myGestureLib; 
+	private GestureOverlayView gOV;
+	private FrameLayout mFL; 
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
-		//call superclass Activity's onCreate to pass Bundle obj
-		super.onCreate(savedInstanceState);
-		//set the layout to the xml defined for gameplay file
-		setContentView(R.layout.activity_gameplay);
-		//create GestureOverlayView obj -- this will encompass the region where the gestures are picked up
-		GestureOverlayView gestureOverlayView = (GestureOverlayView)findViewById(R.id.gestureOverlayView1);
-		//link this guy up to the listener we define for when a gesture happens
-	    gestureOverlayView.addOnGesturePerformedListener(this);
+		super.onCreate(savedInstanceState); //call superclass Activity's onCreate to pass Bundle obj
+		
+		//solution 2
+		gOV = new GestureOverlayView(this); 
+		gameplayView = new SketchtrisView(this); 
+		mFL = new FrameLayout(this);
+		
+		 //gestureOverlayView.addView(surfaceView);    
+        gOV.setOrientation(gOV.ORIENTATION_VERTICAL);
+        gOV.setEventsInterceptionEnabled(true);
+        gOV.setGestureStrokeType(gOV.GESTURE_STROKE_TYPE_MULTIPLE);	
+        //gOV.addOnGesturePerformedListener(this);
 	    //make sure our gestures pre defined to correspond to Tetris shapes are used 
 	    myGestureLib = GestureLibraries.fromRawResource(this, R.raw.gestures);
 	    //error check in case we can't open the gestures file
 	    if (!myGestureLib.load()) {
 	    	finish(); 
 	    }
+        
+	    gOV.addOnGesturePerformedListener(new OnGesturePerformedListener() {
+			@Override
+			//this method fires off when the user has finished inputting the gesture
+			public void onGesturePerformed(GestureOverlayView arg0, Gesture gesture) {
+				Log.v("performed","performed");
+				//as our gestureLibrary tries to recognize drawn shapes, it creates Prediction objects;
+				//Prediction objects basically store a name and a score (score is likelihood of a match)
+				//So predictions is an ArrayList of all our Gestures with the likelihood that we have a match of each one
+			    ArrayList<Prediction> predictions = myGestureLib.recognize(gesture);
+			    for (Prediction p : predictions) {
+			    	//while a higher score value is "better" and helps eliminate false positives, 
+			    	//if we're too strict nobody will ever get one of their drawn shapes recognized.
+			        if (p.score > 4.5 && !(p.name.equals("T"))) {
+			          Toast.makeText(getBaseContext(), p.name, Toast.LENGTH_SHORT).show(); //just simple popup to say shape name, using Tetris letters
+			        }
+			        else {
+			        	if (p.score > 6.5) {
+			        		Toast.makeText(getBaseContext(),p.name,Toast.LENGTH_SHORT).show();
+			        	}
+			        }
+			      }
+			}
+	    });
+        mFL.addView(gameplayView, 0);
+        mFL.addView(gOV,1);
+        setContentView(mFL);
 	}
-
-	@Override
-	//this method fires off when the user has finished inputting the gesture
-	public void onGesturePerformed(GestureOverlayView arg0, Gesture gesture) {
-		//as our gestureLibrary tries to recognize drawn shapes, it creates Prediction objects;
-		//Prediction objects basically store a name and a score (score is likelihood of a match)
-		//So predictions is an ArrayList of all our Gestures with the likelihood that we have a match of each one
-	    ArrayList<Prediction> predictions = myGestureLib.recognize(gesture);
-	    for (Prediction p : predictions) {
-	    	//while a higher score value is "better" and helps eliminate false positives, 
-	    	//if we're too strict nobody will ever get one of their drawn shapes recognized.
-	        if (p.score > 4.5 && !(p.name.equals("T"))) {
-	          Toast.makeText(this, p.name, Toast.LENGTH_SHORT).show(); //just simple popup to say shape name, using Tetris letters
-	        }
-	        else {
-	        	if (p.score > 6.5) {
-	        		Toast.makeText(this,p.name,Toast.LENGTH_SHORT).show();
-	        	}
-	        }
-	      }
-	}
-	
 }
